493a930e073dcf035ed36ad1ae8072aacf64da7b
KanizFatimaMitu <kanizfatima528@gmail.com>
2022-08-26 17:18:42 +0600

    signup setup done

--- src/Components/LogIn/LogIn.js
+++ src/Components/LogIn/LogIn.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import CountDown from '../CountDown/CountDown';
+import { NavLink } from 'react-router-dom';
 import pic from '../../Assests/pic.jpg'
 
 
@@ -11,8 +11,11 @@ const LogIn = () => {
                     <div class="text-center lg:text-left">
                         <h1 class="text-5xl font-bold pb-5 text-primary">Enroll now !</h1>
                         <p>JavaScript is the world's most popular programming language. JavaScript is the programming language of the Web used by programmers across the world to create dynamic and interactive web content like applications and browsers.JavaScript is a simple and easy-to-learn programming language as compared to other languages such as C++, Ruby, and Python. </p>
-                       <CountDown></CountDown>
+                        <button class="btn btn-active mt-2 btn-primary">
+                            <NavLink to='/signup'>Enroll</NavLink>
+                        </button>
                     </div>
+
                     <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl ">
                         <div>
                             <img src={pic} alt='/'></img>
