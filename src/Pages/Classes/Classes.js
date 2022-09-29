1e0af5dc2961cffb2f8d45c8db41fa424f4c8217
KanizFatimaMitu <kanizfatima528@gmail.com>
2022-09-29 20:48:14 +0600

    almost done

--- src/Pages/Classes/Classes.js
+++ src/Pages/Classes/Classes.js
@@ -2,6 +2,8 @@ import React from 'react';
 import About from '../../Pages/About/About'
 import Footer from '../../Shared/Footer/Footer';
 import Questions from '../../Shared/Questions/Questions';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
 
 const Classes = () => {
     return (
@@ -9,7 +11,7 @@ const Classes = () => {
             <div class="flex flex-col w-full lg:flex-row">
                 <div class="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">
                     {/* <!-- The button to open modal --> */}
-                    <label for="question-modal" class="btn modal-button font-bold text-info">Ask Question ?</label>
+                    <label for="question-modal" class="btn modal-button font-bold text-info"> <FontAwesomeIcon icon={faPlusCircle}></FontAwesomeIcon>   Ask Question ?</label>
 
                     {/* <!-- Put this part before </body> tag --> */}
                     <input type="checkbox" id="question-modal" class="modal-toggle" />
@@ -23,29 +25,29 @@ const Classes = () => {
                         </div>
                     </div>
                 </div>
-                
+
                 <div class="divider lg:divider-horizontal text-info">OR</div>
                 <div class="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">
                     {/* <!-- The button to open modal --> */}
-                    <label for="friend-modal" class="btn modal-button font-bold text-info">Add friend ?</label>
+                    <label for="friend-modal" class="btn modal-button font-bold text-info"> <FontAwesomeIcon icon={faPlusCircle}></FontAwesomeIcon> Add friend ?</label>
 
                     {/* <!-- Put this part before </body> tag --> */}
-                <input type="checkbox" id="friend-modal" class="modal-toggle" />
-                <div class="modal modal-bottom sm:modal-middle">
-                    <div class="modal-box">
-                        <h3 class="font-bold text-lg mb-4 text-info">Add your friend here !</h3>
-                        <textarea class="textarea w-full textarea-primary" placeholder="type your friend's email here...."></textarea>
-                        <div class="modal-action">
-                            <label for="friend-modal" class="btn font-bold text-info">Add</label>
+                    <input type="checkbox" id="friend-modal" class="modal-toggle" />
+                    <div class="modal modal-bottom sm:modal-middle">
+                        <div class="modal-box">
+                            <h3 class="font-bold text-lg mb-4 text-info">Add your friend here !</h3>
+                            <textarea class="textarea w-full textarea-primary" placeholder="type your friend's email here...."></textarea>
+                            <div class="modal-action">
+                                <label for="friend-modal" class="btn font-bold text-info">Add</label>
+                            </div>
                         </div>
                     </div>
                 </div>
-                </div>
-        </div>
-        
-        <Questions></Questions>
-        <About></About>
-        <Footer></Footer>
+            </div>
+
+            <Questions></Questions>
+            <About></About>
+            <Footer></Footer>
         </div >
     );
 };
--- src/Shared/Questions/Questions.js
+++ src/Shared/Questions/Questions.js
@@ -1,28 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowsDownToLine } from '@fortawesome/free-solid-svg-icons';
+import { faArrowAltCircleDown } from '@fortawesome/free-solid-svg-icons';
 import { faCheck } from '@fortawesome/free-solid-svg-icons';
 
 const Questions = () => {
-    const [ques, setQues] = useState()
-
-        useEffect(()=>{
-            fetch('')
-        },[])
-
     return (
-        <div className='mx-16 my-8 lg:mx-40 lg:my-16'>
+        <div className='mx-40 my-16'>
             <div class="collapse">
                 <input type="checkbox" class="peer" />
                 <div class="collapse-title  bg-base-300 text-gray-300 font-bold text-center text-xl peer-checked:bg-neutral peer-checked:text-info">
-                    <FontAwesomeIcon icon={ faArrowsDownToLine }></FontAwesomeIcon>  Click me to show all recent questions
+                    <FontAwesomeIcon icon={ faArrowAltCircleDown }></FontAwesomeIcon>  Click me to show all recent questions
                 </div>
                 <div class="collapse-content bg-primary font-bold text-primary peer-checked:bg-neutral peer-checked:text-info">
                     <div class="collapse-title my-2 text-xl bg-primary text-white text- font-medium">
                        <FontAwesomeIcon icon= { faCheck }></FontAwesomeIcon> what is database ?
                     </div>
                     <div class="collapse-title my-2 text-xl bg-primary text-white text- font-medium">
-                       <FontAwesomeIcon icon= { faCheck }></FontAwesomeIcon> what is the Difference between query & params ?
+                       <FontAwesomeIcon icon= { faCheck }></FontAwesomeIcon> what is the Difference between "===" & "==" ?
+                    </div>
+                    <div class="collapse-title my-2 text-xl bg-primary text-white text- font-medium">
+                       <FontAwesomeIcon icon= { faCheck }></FontAwesomeIcon> what is Node.js
                     </div>
                 </div>
             </div>
